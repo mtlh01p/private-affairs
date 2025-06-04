da7603f508c485285746ddc283e373d9a612446f
Matthew Loho <118585313+mtlh01p@users.noreply.github.com>
2025-06-04 12:33:59 +0800

    Update BitCount.py

--- learning/10 Apr 2025/BitCount.py
+++ learning/10 Apr 2025/BitCount.py
@@ -2,11 +2,16 @@ T = int(input())
 for i in range(T):
     ans = 0
     X = int(input())
+    lower_lim = 0
     for j in range(1, X+1):
         j_bin = len(bin(j))-2
         div = X//j
         div_bin = len(bin(div))-2
         if div_bin <= j_bin:
             ans += 1
+            lower_lim = j
+            break
+    ans += X-lower_lim
     print(ans)
+
 
