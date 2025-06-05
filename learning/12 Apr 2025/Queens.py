bfbe22721f58e07dd04077380ced37221c881cda
Matthew Loho <118585313+mtlh01p@users.noreply.github.com>
2025-06-05 15:55:01 +0800

    EW18 Redhill

---
+++ learning/12 Apr 2025/Queens.py
@@ -0,0 +1,31 @@
+def is_safe(board, row, col, n):
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+        if col - (row - i) >= 0 and board[i][col - (row - i)] == 1:
+            return False
+        if col + (row - i) < n and board[i][col + (row - i)] == 1:
+            return False
+    return True
+
+def solve_n_queens(board, row, n):
+    if row == n:
+        return True
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1
+            if solve_n_queens(board, row + 1, n):
+                return True
+            board[row][col] = 0 
+    return False
+
+def n_queens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    if solve_n_queens(board, 0, n):
+        for row in board:
+            print(' '.join(map(str, row)))
+    else:
+        print("Not possible")
+
+n = int(input())
+n_queens(n)
